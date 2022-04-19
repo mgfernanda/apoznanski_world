634c07473426499c86d7517f6b57f5f9da1ef41e
Fernanda <fmolinag@andrew.cmu.edu>
2022-04-19 19:35:48 -0400

    Create model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     belongs_to :category
 
     validates_presence_of :title
-    validates_presence_of :conten
+    validates_presence_of :content
 
     scope :alphabetical, -> { order('title') }
     scope :active, -> { where('active = ?', true) }
